4b4c242386beb0167d6e6c16cee77fd32279603a
Matt <mawburn7@gmail.com>
2017-07-09 20:02:22 -0500

    working server
    based on: https://github.com/fullstackreact/food-lookup-demo

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const request = require('request')
+const cheerio = require('cheerio')
+
+app.set("port", process.env.PORT || 3001)
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+}
+
+app.get("/api/title", (req, res) => {
+  const url = req.query.url
+
+  if (!url) { 
+    res.json({error: "Missing url"})
+    return 
+  }
+
+  request.get(url, (err, response, body) => {
+    if(!err && response.statusCode === 200) {
+      const $ = cheerio.load(body)
+      const title = $('title').text()
+      res.send({title})
+    }
+  })
+})
+
+app.listen(app.get("port"), () => {
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`) // eslint-disable-line no-console
+})
